59af62c6b35120ec02215aa4a5cc499a0532ccbc
ARRY7686 <aadigupta2007@yahoo.com>
2025-03-17 01:13:26 +0530

    implement board square enumeration and enhance bit manipulation macros

--- src/core/Board.cpp
+++ src/core/Board.cpp
@@ -4,9 +4,22 @@
 //defining bitboard types
 #define U64 unsigned long long
 
+//board squares
+enum{
+    a8,b8,c8,d8,e8,f8,g8,h8,
+    a7,b7,c7,d7,e7,f7,g7,h7,
+    a6,b6,c6,d6,e6,f6,g6,h6,
+    a5,b5,c5,d5,e5,f5,g5,h5,
+    a4,b4,c4,d4,e4,f4,g4,h4,
+    a3,b3,c3,d3,e3,f3,g3,h3,
+    a2,b2,c2,d2,e2,f2,g2,h2,
+    a1,b1,c1,d1,e1,f1,g1,h1
+};
+
 // defining set/get/pop macros
 #define getBit(bitBoard,square) (bitBoard & (1ULL << square))
-
+#define setBit(bitBoard,square) (bitBoard |= (1ULL << square))
+#define popBit(bitBoard,square) (getBit(bitBoard,square)? bitBoard ^= (1ULL << square) : 0)
 
 //printing bitboard function(looping over files and ranks)
 
@@ -33,7 +46,12 @@ void printBitBoard(U64 bitBoard){
 
 //Main driver function
 int main(){
-    U64 bitboard = 1ULL;
-    printBitBoard(bitboard);
+    //defining bitboard
+    U64 bitBoard = 0ULL;
+    //using the macros manipulating it and printing it
+    setBit(bitBoard,e2);
+    setBit(bitBoard,e4);
+    popBit(bitBoard,e2);
+    printBitBoard(bitBoard);
     return 0;
 }
